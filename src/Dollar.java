98b66fc56f2bd3b3d980e7c0ae878e9e0403ab2a
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2020-10-05 19:50:57 -0300

    Segundo passo

---
+++ src/Dollar.java
@@ -0,0 +1,9 @@
+class Dollar extends Money {
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
---
+++ src/TestFranc.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+class TestMoney {
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+}
